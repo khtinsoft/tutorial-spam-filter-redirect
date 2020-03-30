5d78eff61b2a0591f01590782bdf8944f49b22b9
ted <ted@gelato.im>
2020-03-30 23:28:38 +0900

    html 문서 내 a 링크에 대해, 상대 주소에 대한 처리 적요 방문한 페이지에 대한 cache 적용

--- app.py
+++ app.py
@@ -14,40 +14,64 @@ RESPONSE_PAGE = [200]
 already_visited = {}
 already_visited_lock = asyncio.Lock()
 
-def detect_url_in_content(content: str):        
-    url = re.search(r"(?P<url>https?://[^\s]+)", content).group("url")
-    return url 
+def detect_urls_in_content(content: str) -> str:        
+    urls = list(re.findall(r"https?://[^\s]+", content))
+    return urls
 
 
-def detect_url_in_document(content: str):
-    soup = BeautifulSoup(content, "html.parser")
+def detect_urls_in_document(document: str, url: str) -> List[str]:
+    
+    url_parts = parse.urlparse(url)
+    soup = BeautifulSoup(document, "html.parser")
+    links = list(soup.select("a"))
 
-    filtered = filter(
-        lambda item: item.get("href", None) is not None,
-        soup.select("a")
-    )
 
-    return map(
-        lambda item: item.get("href"),
-        filtered
-    )
+    # document 내에 a 링크에 대한 처리
+    # 상대 적인 좌표의 경우, 현재 url scheme 및 netloc 사용
+    urls = []
+    for item in links:          
+        
+        href = item.get("href", None)          
+        
+        if not href:
+            continue
+            
+        parts = parse.urlparse("{}".format(href))
+        if not parts.path:
+            continue     
 
-def get_next_urls(response):
+        if parts.scheme not in ['http', 'https']:
+            continue   
+    
+        updated_parts = parse.ParseResult(
+            scheme=parts.scheme if parts.scheme else url_parts.scheme,
+            netloc=parts.netloc if parts.netloc else url_parts.netloc,
+            path=parts.path,
+            query=parts.query,
+            params=parts.params,
+            fragment=parts.fragment
+        )
+        urls.append(parse.urlunparse(updated_parts))
+
+    return urls
+
+
+def get_next_urls(response: requests.Response) -> List[str]:
     if response.status_code in RESPONSE_REDIRECTS:
         next_urls = [response.headers.get("Location")]
     elif response.status_code in RESPONSE_PAGE:
-        next_urls = detect_url_in_document(response.text)
+        next_urls = detect_urls_in_document(document=response.text, url=response.request.url)
     else:
-        raise ValueError
+        next_urls = []
 
     return next_urls
 
-def check_url_in_domains(url, domains):
+def check_url_in_domains(url: str, domains: List[str]) -> bool:
     url_parts = parse.urlparse(url)
     return True if url_parts.netloc in domains else False      
 
 
-async def get_response(url):
+async def get_response(url: str) -> requests.Response:
 
     response = requests.get(
         url=url,
@@ -56,22 +80,26 @@ async def get_response(url):
         
     return response
 
+async def get_response_cache(url: str, visited) -> requests.Response:
+    return visited[url]
 
-async def is_spam(content: str, spam_link_domains: List[str], redirectionDepth: int):
+
+async def is_spam(content: str, spam_link_domains: List[str], redirectionDepth: int) -> bool:
     
-    next_urls = [detect_url_in_content(content)]
+    next_urls = detect_urls_in_content(content)
     visited = {}
         
     while redirectionDepth >= 0:
-        futures = []
+        futures = []                
 
-        for next_url in next_urls:
+        for next_url in next_urls:            
             in_spam = check_url_in_domains(next_url, spam_link_domains)
             if in_spam:
                 return True
 
+            # 기존에 Visited 한 url 은 다시 방문하지 않고, 응답에 대한 cache 사용
             if next_url in visited:
-                futures.append(asyncio.ensure_future(lambda: visited[next_url]))
+                futures.append(asyncio.ensure_future(get_response_cache(next_url, visited)))
             else:            
                 futures.append(asyncio.ensure_future(get_response(next_url)))
 
@@ -83,6 +111,7 @@ async def is_spam(content: str, spam_link_domains: List[str], redirectionDepth: 
             except:
                 continue
 
+            # 방문한 페이지에 대해 cache 설정
             visited[response.request.url] = response
 
         redirectionDepth -= 1
@@ -96,13 +125,14 @@ async def is_spam(content: str, spam_link_domains: List[str], redirectionDepth: 
 def main():
     loop = asyncio.get_event_loop()    
 
+    print(loop.run_until_complete(is_spam("spam spam https://m.gelatofactory.co.kr/goods/event_sale_list.php", ["naver.me"], 1))) # true
     print(loop.run_until_complete(is_spam("spam spam https://unurl.kr/99a7", ["bit.ly"], 1))) # true
     print(loop.run_until_complete(is_spam("spam spam https://unurl.kr/99a7", ["unurl.kr"], 1))) # true
     print(loop.run_until_complete(is_spam("spam spam https://unurl.kr/99a7", ["tvtv24.com"], 1))) # false
 
     print(loop.run_until_complete(is_spam("spam spam https://unurl.kr/99a7", ["bit.ly"], 2))) # true
     print(loop.run_until_complete(is_spam("spam spam https://unurl.kr/99a7", ["unurl.kr"], 2))) # true
-    print(loop.run_until_complete(is_spam("spam spam https://unurl.kr/99a7", ["tvtv24.com"], 2))) # false
+    print(loop.run_until_complete(is_spam("spam spam https://unurl.kr/99a7", ["tvtv24.com"], 2))) # true
 
 main()
 
